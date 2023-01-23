bfb1b8f6c63b974b41415f1880dacf26b9adbde3
Md Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2023-01-24 03:35:41 +0600

    update

--- controller/userController.js
+++ controller/userController.js
@@ -3,9 +3,13 @@
 // login
 export const login = async (req, res) => {
     // save to session
-    const { user } = req.session;
-    req.session.user = !user ? Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) : user;
-    return res.json({ message: "Logged in" });
+    try {
+        const { user } = req.session;
+        req.session.user = !user ? Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) : user;
+        return res.json({ message: "Logged in" });
+    } catch (error) {
+        return res.status(500).json({ message: "Server error", error: error });
+    }
 
 }
 
